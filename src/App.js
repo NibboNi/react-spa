768facb956e366b91481d389f6c80d68057dc967
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-09-12 18:29:56 -0600

    Add movies rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
-    
-const Movie1 = {
-    "Title": "The Road Warrior",
-    "Year": "1981",
-    "imdbID": "tt0082694",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BN2VlNjNhZWQtMTY2OC00Y2E1LWJkNGUtMDU4M2ViNzliMGYwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  }
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data.Search);
+
+        setMovies(data.Search);
     };
 
     useEffect(() => {
@@ -38,20 +34,26 @@ const App = () => {
                     onClick={() => {}}
                 />
             </div>
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{Movie1.Year}</p>
+            {
+                movies?.length > 0
+                ?
+                (
+                    <div className="container">
+                        {
+                            movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))
+                        }
+                        <MovieCard movie={movies[0]}/>
                     </div>
-                    <div>
-                        <img src={Movie1.Poster !== "N/A" ? Movie1.Poster : "https://source.unsplash.com/qGbo4o77NaM"} alt={Movie1.title}/>
+                )
+                :
+                (
+                    <div className="empty">
+                        <h2>No movies found</h2>
                     </div>
-                    <div>
-                        <span>{Movie1.Type}</span>
-                        <h3>{Movie1.Title} ({Movie1.Year})</h3>
-                    </div>
-                </div>
-            </div>
+                )
+            }
         </div>
     );
 }
